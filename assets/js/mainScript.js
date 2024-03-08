f50359884053c22c7f95ce890cb17e1c64c67178
Adham Kaseb <adhamkasebssj4@gmail.comm>
2024-03-09 00:01:41 +0200

    i ma happy right now !

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -17,12 +17,56 @@ titles.forEach((title, index) => {
   title.addEventListener("click", () => {
     var icon = icons[index];
     // Toggle rotation
-    icon.style.transform = icon.style.transform === "rotate(90deg)" ? "rotate(0deg)" : "rotate(90deg)";
+    icon.style.transform =
+      icon.style.transform === "rotate(90deg)"
+        ? "rotate(0deg)"
+        : "rotate(90deg)";
   });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  setTimeout(function() {
-    document.querySelector('.preloader').style.display = 'none';
-  }, 2000); 
+setTimeout(function () {
+  document.querySelector(".preloader").style.display = "none";
+}, 2600);
+
+
+
+
+
+
+
+
+// editor related area
+const ed_link1 = document.querySelector(".ed_link1");
+const content = document.querySelectorAll(".content");
+
+const ed_link2 = document.querySelector(".ed_link2");
+const editor_related_mustRead = document.querySelector(".editor_related_mustRead");
+
+const ed_link3 = document.querySelector(".ed_link3");
+const editor_related_contact = document.querySelector(".editor_related_contact");
+
+ed_link1.addEventListener("click", function() {
+  content.style.height = "100%";
+  editor_related_mustRead.style.height = "0px";
+  editor_related_mustRead.style.left = "-150%";
+  editor_related_contact.style.height = "0px";
+  editor_related_contact.style.left = "-150%";
+});
+
+ed_link2.addEventListener("click", function() {
+  editor_related_mustRead.style.height = "100%";
+  editor_related_mustRead.style.left = "0";
+  editor_related_contact.style.height = "0px";
+  editor_related_contact.style.left = "-150%";
+  content.style.height = "0px !important";
+  content.style.left = "-150%";
+});
+
+ed_link3.addEventListener("click", function() {
+  editor_related_contact.style.height = "100%";
+  editor_related_contact.style.left = "0";
+  editor_related_mustRead.style.height = "0px";
+  editor_related_mustRead.style.left = "-150%";
+  content.style.height = "0px";
+  content.style.left = "-150%";
 });
